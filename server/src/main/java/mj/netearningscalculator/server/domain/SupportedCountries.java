ddaa2cbd8777bd0565e95a0470bf55d8c5738c0d
mjazy <112projects.contact@gmail.com.com>
2018-11-29 01:05:56 +0100

    9th commit: SupportedCountries class added

---
+++ server/src/main/java/mj/netearningscalculator/server/domain/SupportedCountries.java
@@ -0,0 +1,71 @@
+package mj.netearningscalculator.server.domain;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Class maintaining currently supported countries. Currently supported
+ * countries are Germany, Poland, United Kingdom.
+ * 
+ * @author MJazy
+ *
+ */
+@Component("supportedCountries")
+public class SupportedCountries {
+
+	private List<Country> supportedCountriesList;
+
+	/**
+	 * Constructor for SupportedCountries class.
+	 */
+	public SupportedCountries() {
+		setSupportedCountriesList(new ArrayList<Country>());
+
+		CountryFinances germanyFinances = new CountryFinances("EUR", new BigDecimal("0.20"), new BigDecimal("800"));
+		Country germany = new Country("Germany", "DE", germanyFinances);
+
+		CountryFinances polandFinances = new CountryFinances("PLN", new BigDecimal("0.19"), new BigDecimal("1200"));
+		Country poland = new Country("Poland", "PL", polandFinances);
+
+		CountryFinances unitedKingdomFinances = new CountryFinances("GBP", new BigDecimal("0.25"),
+				new BigDecimal("600"));
+		Country unitedKingdom = new Country("United Kingdom", "UK", unitedKingdomFinances);
+
+		getSupportedCountriesList().add(germany);
+		getSupportedCountriesList().add(poland);
+		getSupportedCountriesList().add(unitedKingdom);
+
+	}
+
+	public List<Country> getSupportedCountriesList() {
+		return supportedCountriesList;
+	}
+
+	public void setSupportedCountriesList(List<Country> supportedCountriesList) {
+		this.supportedCountriesList = supportedCountriesList;
+	}
+
+	/**
+	 * Confirms if country is supported.
+	 * 
+	 * @param countryCode compliant with ISO 3166-1 e.g. 'PL'.
+	 * @return true if country is supported, false if else.
+	 */
+	public boolean containsCountry(String countryCode) {
+		for (Country country : supportedCountriesList) {
+			if (country.getCode().equals(countryCode)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("supportedCountries: '%s'", supportedCountriesList.toString());
+	}
+
+}
---
+++ server/src/test/java/mj/netearningscalculator/server/domain/SupportedCountriesTest.java
@@ -0,0 +1,38 @@
+package mj.netearningscalculator.server.domain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import javax.inject.Inject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SupportedCountriesTest {
+
+	@Inject
+	SupportedCountries supportedCountries;
+
+	@Test
+	public void containsCountryTest() {
+		assertTrue(supportedCountries.containsCountry("DE"));
+		assertTrue(supportedCountries.containsCountry("PL"));
+		assertTrue(supportedCountries.containsCountry("UK"));
+
+		assertFalse(supportedCountries.containsCountry("FR"));
+		assertFalse(supportedCountries.containsCountry(null));
+	}
+
+	@Test
+	public void toStringTest() {
+		String relevantToStringValue = String.format("supportedCountries: '%s'",
+				supportedCountries.getSupportedCountriesList().toString());
+		assertEquals(supportedCountries.toString(), relevantToStringValue);
+	}
+
+}
