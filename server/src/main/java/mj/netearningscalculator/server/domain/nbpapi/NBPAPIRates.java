2501df2b55fbd01158589367e01ac665f54b55cd
mjazy <112projects.contact@gmail.com.com>
2018-11-30 22:21:28 +0100

    14th commit: NBPAPIrates data model added

---
+++ server/src/main/java/mj/netearningscalculator/server/domain/nbpapi/NBPAPIRates.java
@@ -0,0 +1,61 @@
+package mj.netearningscalculator.server.domain.nbpapi;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Data model for rates returned as part of response from NBP API.
+ * 
+ * @author MJazy
+ *
+ */
+public class NBPAPIRates {
+
+	@JsonProperty
+	private String no;
+	@JsonProperty
+	private Date effectiveDate;
+	@JsonProperty
+	private BigDecimal bid;
+	@JsonProperty
+	private BigDecimal ask;
+
+	/**
+	 * Constructor for NBPRates class.
+	 * 
+	 * @param no            e.g. '233/C/NBP/2018'.
+	 * @param effectiveDate e.g. '2018-11-30'.
+	 * @param bid           e.g. '3.7298'.
+	 * @param ask           e.g. '3.8052'.
+	 */
+	public NBPAPIRates(String no, Date effectiveDate, BigDecimal bid, BigDecimal ask) {
+		this.no = no;
+		this.effectiveDate = effectiveDate;
+		this.bid = bid;
+		this.ask = ask;
+	}
+
+	public String getNo() {
+		return no;
+	}
+
+	public Date getEffectiveDate() {
+		return effectiveDate;
+	}
+
+	public BigDecimal getBid() {
+		return bid;
+	}
+
+	public BigDecimal getAsk() {
+		return ask;
+	}
+
+	public String toString() {
+		return String.format("no: '%s', effectiveDate: '%s', bid: '%s', ask: '%s'", this.no, this.effectiveDate,
+				this.bid, this.ask);
+	}
+
+}
---
+++ server/src/test/java/mj/netearningscalculator/server/domain/nbpapi/NBPAPIRatesTest.java
@@ -0,0 +1,33 @@
+package mj.netearningscalculator.server.domain.nbpapi;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NBPAPIRatesTest {
+
+	NBPAPIRates nbpRates;
+	String relevantToStringValue;
+
+	@Before
+	public void initializeVariables() {
+		String testNo = "233/C/NBP/2018";
+		Date testDate = new Date(10000000);
+		BigDecimal testBid = new BigDecimal("4.25");
+		BigDecimal testAsk = new BigDecimal("4.27");
+
+		nbpRates = new NBPAPIRates(testNo, testDate, testBid, testAsk);
+		relevantToStringValue = String.format("no: '%s', effectiveDate: '%s', bid: '%s', ask: '%s'", testNo,
+				testDate, testBid, testAsk);
+	}
+
+	@Test
+	public void toStringTest() {
+		assertEquals(nbpRates.toString(), relevantToStringValue);
+	}
+
+}
