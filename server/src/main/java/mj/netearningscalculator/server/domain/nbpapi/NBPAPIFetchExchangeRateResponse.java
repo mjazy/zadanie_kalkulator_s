e4743864d29be8f3ea9093a904754b1efbfb9403
mjazy <112projects.contact@gmail.com.com>
2018-11-30 22:22:04 +0100

    15th commit: NBPAPIFetchExchangeRateResponse data model added

---
+++ server/src/main/java/mj/netearningscalculator/server/domain/nbpapi/NBPAPIFetchExchangeRateResponse.java
@@ -0,0 +1,65 @@
+package mj.netearningscalculator.server.domain.nbpapi;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Data model for response from NBP API to request related to fetching exchange
+ * rate.
+ */
+public class NBPAPIFetchExchangeRateResponse {
+
+	@JsonProperty
+	private char table;
+
+	@JsonProperty
+	private String currency;
+
+	@JsonProperty
+	private String code;
+
+	@JsonProperty
+	private NBPAPIRates rates;
+
+	/**
+	 * Constructor for NBPAPIFetchExchangeRateResponse class.
+	 * 
+	 * @param table    e.g. 'A'.
+	 * @param currency e.g. 'euro'.
+	 * @param code     compliant with ISO 4127 e.g. 'EUR'.
+	 * @param rates    {@link NBPAPIRates#NBPAPIRates(String, java.util.Date, java.math.BigDecimal, java.math.BigDecimal)}
+	 */
+	public NBPAPIFetchExchangeRateResponse(char table, String currency, String code, NBPAPIRates rates) {
+		this.table = table;
+		this.currency = currency;
+		this.code = code;
+		this.rates = rates;
+	}
+
+	public char getTable() {
+		return table;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	/**
+	 * Gets currency code.
+	 * 
+	 * @return code compliant with ISO 4127 e.g. 'EUR'.
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	public NBPAPIRates getRates() {
+		return rates;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("table: '%s', currency: '%s', code: '%s', %s", this.table, this.currency, this.code,
+				this.rates);
+	}
+
+}
---
+++ server/src/test/java/mj/netearningscalculator/server/domain/nbpapi/NBPAPIFetchExchangeRateResponseTest.java
@@ -0,0 +1,37 @@
+package mj.netearningscalculator.server.domain.nbpapi;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NBPAPIFetchExchangeRateResponseTest {
+
+	NBPAPIFetchExchangeRateResponse nbpAPIFetchExchangeRateResponse;
+	String relevantToStringValue;
+
+	@Before
+	public void initializeVariables() {
+
+		NBPAPIRates nbpPAPIRates = new NBPAPIRates("233/C/NBP/2018", new Date(10000000), new BigDecimal("4.25"),
+				new BigDecimal("4.27"));
+
+		char table = 'a';
+		String currency = "Euro";
+		String code = "EUR";
+
+		nbpAPIFetchExchangeRateResponse = new NBPAPIFetchExchangeRateResponse(table, currency, code,
+				nbpPAPIRates);
+		relevantToStringValue = String.format("table: '%s', currency: '%s', code: '%s', %s", table, currency,
+				code, nbpPAPIRates);
+	}
+	
+	@Test
+	public void toStringTest() {
+		assertEquals(nbpAPIFetchExchangeRateResponse.toString(), relevantToStringValue);
+	}
+
+}
