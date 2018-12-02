bd8f8301b60a68ca8c282d467da362b842dad09e
mjazy <112projects.contact@gmail.com.com>
2018-12-02 20:01:54 +0100

    20th commit: ExchangeRateFetcherInterface updated

--- server/src/main/java/mj/netearningscalculator/server/service/ExchangeRateFetcherInterface.java
+++ server/src/main/java/mj/netearningscalculator/server/service/ExchangeRateFetcherInterface.java
@@ -12,7 +12,7 @@ public interface ExchangeRateFetcherInterface {
 	/**
 	 * Method for fetching exchange rate between supplied Currency and PLN (bid).
 	 * @param currency compliant with ISO 4217 e.g. 'EUR.
-	 * @return exchange rate returned by API.
+	 * @return exchange rate returned by API or '-1' in case issue appeared.
 	 */
 	BigDecimal fetchExchangeRate(String currency);
 
