f08e3492bc30ec53e631d9042b1636d506aef20c
mjazy <112projects.contact@gmail.com.com>
2018-12-11 16:01:52 +0100

    37th commit: CrossOrigin annotation added in ApplicationController

--- server/src/main/java/mj/netearningscalculator/server/rest/ApplicationController.java
+++ server/src/main/java/mj/netearningscalculator/server/rest/ApplicationController.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,23 +17,27 @@ import mj.netearningscalculator.server.domain.SupportedCountries;
 import mj.netearningscalculator.server.service.NetEarningsService;
 
 /**
- * Controller for application.
+ * Controller for application. Values of origins are of Client module (where
+ * port 4200 is of application run, and 9876 of Karma's tests).
  * 
  * @author MJazy
  *
  */
+@CrossOrigin(origins = { "http://localhost:4200", "http://localhost:9876" })
 @RestController
 public class ApplicationController {
 
 	@Inject
 	NetEarningsService netEarningsService;
-	
+
 	@Inject
 	SupportedCountries supportedCountries;
 
 	/**
 	 * GET endpoint for calculating monthly net earnings in PLN.
-	 * @param countryCode compliant with ISO 3166-1 and supported countries list.
+	 * 
+	 * @param countryCode        compliant with ISO 3166-1 and supported countries
+	 *                           list.
 	 * @param grossDailyEarnings relevant for country of request's currency.
 	 * @return monthly net earnings in PLN or relevant error communicate.
 	 */
@@ -41,9 +46,9 @@ public class ApplicationController {
 			@PathVariable BigDecimal grossDailyEarnings) {
 		return netEarningsService.runService(countryCode, grossDailyEarnings);
 	}
-	
+
 	@GetMapping("/countries")
-	ResponseEntity<List<Country>> getSupportedCountries(){
+	ResponseEntity<List<Country>> getSupportedCountries() {
 		return ResponseEntity.status(HttpStatus.OK).body(supportedCountries.getSupportedCountriesList());
 	}
 }
