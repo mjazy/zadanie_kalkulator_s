d74e7240a0bf3d2e85fa23c752a2ac6ce4bbb58c
mjazy <112projects.contact@gmail.com.com>
2018-12-11 15:40:43 +0100

    34th commit: client module's title changed

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'client'`, () => {
+  it(`should have as title 'NetEarningsCalculator'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('client');
+    expect(app.title).toEqual('NetEarningsCalculator');
   });
 
   it('should render title in a h1 tag', () => {
